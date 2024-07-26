ebf891d7e77a15b74c76e2ad754f8001d0a08c3d
agnaldo123 <105053109+agnaldo123@users.noreply.github.com>
2024-07-26 13:00:17 +0000

    .

--- JS main.js
+++ JS main.js
@@ -1,7 +1,27 @@
 const pergunta =[
     pergunta1{
-    enunciado,
+    enunciado: "voce gosta de bolo?"
+    alternativas:[
+        {
+            texto:"alternativa A",
+            afirmacao:"você e uma pessoa normal"
+        }, 
+        {
+            texto:"alternativa B",
+            afirmacao:"você e chato pra caranbolas"
+        }]
     },
 
-    pergunta2,
+    pergunta2,{enunciado: "voce dorme?"
+        alternativas:[
+            {
+                texto:"sim",
+                afirmacao:"você e uma pessoa normal"
+            }, 
+            {
+                texto:"não",
+                afirmacao:"você é uma anomalia"
+            }]
+
+    }
 ]
